757042cef43ca44d73edaed2ecc39f39dd55c0a9
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-04-02 23:13:44 -0300

    Pedidos com valor maior que 500

--- src/components/OrdensTransporte.vue
+++ src/components/OrdensTransporte.vue
@@ -1,120 +1,146 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols=12>
-      <template>
-        <v-data-table
-          :headers="headers"
-          :items="desserts"
-          sort-by="calories"
-          class="elevation-1"
-        >
-          <template v-slot:top>
-            <v-toolbar flat>
-              <v-toolbar-title>Ordens Transporte</v-toolbar-title>
-              <v-divider class="mx-4" inset vertical></v-divider>
-              <v-spacer></v-spacer>
-              <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    color="primary"
-                    dark
-                    class="mb-2"
-                    v-bind="attrs"
-                    v-on="on"
-                  >
-                    Novo Pedido
-                  </v-btn>
-                </template>
-                <v-card>
-                  <v-card-title>
-                    <span class="headline">{{ formTitle }}</span>
-                  </v-card-title>
+      <v-col cols="12">
+        <template>
+          <v-data-table
+            :headers="headers"
+            :items="pedidos"
+            sort-by="calories"
+            class="elevation-1"
+          >
+            <template v-slot:top>
+              <v-toolbar flat>
+                <v-toolbar-title
+                  >Pedidos com Valor Total acima de 500</v-toolbar-title
+                >
+                <v-divider class="mx-4" inset vertical></v-divider>
+                <v-spacer></v-spacer>
+                <v-dialog v-model="dialog" max-width="500px">
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">{{ formTitle }}</span>
+                    </v-card-title>
 
-                  <v-card-text>
-                    <v-container>
-                      <v-row>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field
-                            v-model="editedItem.name"
-                            label="Dessert name"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field
-                            v-model="editedItem.calories"
-                            label="Calories"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field
-                            v-model="editedItem.fat"
-                            label="Fat (g)"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field
-                            v-model="editedItem.carbs"
-                            label="Carbs (g)"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field
-                            v-model="editedItem.protein"
-                            label="Protein (g)"
-                          ></v-text-field>
-                        </v-col>
-                      </v-row>
-                    </v-container>
-                  </v-card-text>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="8">
+                            <v-text-field
+                              v-model="produtoItem.product1"
+                              counter="50"
+                              label="Produto 1"
+                              :rules="produtoRules"
+                              prepend-icon="mdi-archive"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              v-model="produtoItem.valueProduct1"
+                              label="Valor Produto 1"
+                              @input="calcular"
+                              type="number"
+                              :rules="valorRules"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="8">
+                            <v-text-field
+                              counter="50"
+                              v-model="produtoItem.product2"
+                              prepend-icon="mdi-archive"
+                              label="Produto 2"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              v-model="produtoItem.valueProduct2"
+                              label="Valor Produto 2"
+                              @input="calcular"
+                              type="number"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="8">
+                            <v-text-field
+                              counter="50"
+                              v-model="produtoItem.product3"
+                              prepend-icon="mdi-archive"
+                              label="Produto 3"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              v-model="produtoItem.valueProduct3"
+                              label="Valor Produto 3"
+                              @input="calcular"
+                              type="number"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="12">
+                            <v-text-field
+                              v-model="produtoItem.totalValue"
+                              label="Valor Total"
+                              prepend-icon="mdi-currency-brl"
+                              disabled
+                              type="number"
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
 
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="close">
-                      Cancel
-                    </v-btn>
-                    <v-btn color="blue darken-1" text @click="save">
-                      Save
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-              <v-dialog v-model="dialogDelete" max-width="500px">
-                <v-card>
-                  <v-card-title class="headline"
-                    >Are you sure you want to delete this item?</v-card-title
-                  >
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="closeDelete"
-                      >Cancel</v-btn
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="close">
+                        Sair
+                      </v-btn>
+                      <v-btn color="blue darken-1" text @click="save">
+                        Salvar
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+                <v-dialog v-model="dialogDelete" max-width="500px">
+                  <v-card>
+                    <v-card-title class="headline"
+                      >Are you sure you want to delete this item?</v-card-title
                     >
-                    <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                      >OK</v-btn
-                    >
-                    <v-spacer></v-spacer>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-toolbar>
-          </template>
-          <template v-slot:item.actions="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">
-              mdi-pencil
-            </v-icon>
-            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-          </template>
-          <template v-slot:no-data>
-            <v-btn color="primary" @click="initialize"> Reset </v-btn>
-          </template>
-        </v-data-table>
-      </template>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="closeDelete"
+                        >Cancel</v-btn
+                      >
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="deleteItemConfirm"
+                        >OK</v-btn
+                      >
+                      <v-spacer></v-spacer>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-toolbar>
+            </template>
+            <template v-slot:item.actions="{ item }">
+              <v-icon small class="mr-2" @click="editItem(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            </template>
+            <template v-slot:no-data>
+              <v-btn color="primary" @click="pegarPedidos500">
+                Tentar Novamente
+              </v-btn>
+            </template>
+          </v-data-table>
+        </template>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
+import apiService from "@/services/apiService";
 import Vue from "vue";
 
 export default Vue.extend({
@@ -127,35 +153,41 @@ export default Vue.extend({
         text: "ID",
         align: "start",
         sortable: false,
-        value: "name",
+        value: "id",
       },
-      { text: "ID Pedido", value: "calories" },
-      { text: "Produtos", value: "fat" },
-      { text: "Valor Total", value: "carbs" },
-      { text: "Status", value: "protein" },
+      { text: "Produto 1", value: "product1", sortable: false },
+      { text: "Produto 2", value: "product2", sortable: false },
+      { text: "Produto 3", value: "product3", sortable: false },
+      { text: "Valor Total", value: "totalValue" },
       { text: "Ações", value: "actions", sortable: false },
     ],
-    desserts: [] as any,
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+    pedidos: [] as any,
+    produtoIndex: -1,
+    produtoItem: {
+      id: 0,
+      product1: "",
+      product2: "",
+      product3: "",
+      valueProduct1: 0,
+      valueProduct2: 0,
+      valueProduct3: 0,
+      totalValue: 0,
     },
-    defaultItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+    defaultProduto: {
+      id: 0,
+      product1: "",
+      product2: "",
+      product3: "",
+      valueProduct1: 0,
+      valueProduct2: 0,
+      valueProduct3: 0,
+      totalValue: 0,
     },
   }),
 
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+      return this.produtoIndex === -1 ? "Novo Pedido" : "Editar Pedido";
     },
   },
 
@@ -168,124 +200,54 @@ export default Vue.extend({
     },
   },
 
-  created() {
-    this.initialize();
+  async created() {
+    this.pegarPedidos500();
   },
 
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-        },
-      ];
+    async pegarPedidos500() {
+      apiService.getOrdersAbove500().then((response) => {
+        this.pedidos = response.data;
+      });
     },
 
     editItem(item: any) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
+      this.produtoItem = Object.assign({}, item);
       this.dialog = true;
     },
 
     deleteItem(item: any) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
+      this.produtoItem = Object.assign({}, item);
       this.dialogDelete = true;
     },
 
     deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
       this.closeDelete();
     },
 
     close() {
       this.dialog = false;
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
+        this.produtoItem = Object.assign({}, this.defaultItem);
       });
     },
-
+    calcular() {
+      this.produtoItem.totalValue =
+        parseInt(this.produtoItem.valueProduct1) +
+        parseInt(this.produtoItem.valueProduct2) +
+        parseInt(this.produtoItem.valueProduct3);
+    },
     closeDelete() {
       this.dialogDelete = false;
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
+        this.produtoItem = Object.assign({}, this.defaultItem);
       });
     },
-
     save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      if (this.produtoIndex > -1) {
+        Object.assign(this.pedidos[this.produtoIndex], this.produtoItem);
       } else {
-        this.desserts.push(this.editedItem);
+        this.pedidos.push(this.produtoItem);
       }
       this.close();
     },
